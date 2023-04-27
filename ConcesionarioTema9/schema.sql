403aa6df3cece94445074f9e7978113ae42c99c8
cumpli <cumpli@pc201-13.14005298.14.andared.ced.junta-andalucia.es>
2023-04-27 10:32:44 +0200

    Creacion de los archivos schema y data .sql jueves 27/04/2023 08:30 a 10:30

---
+++ ConcesionarioTema9/schema.sql
@@ -0,0 +1,32 @@
+drop database if exists concesionario;
+            create database concesionario;
+            use concesionario;
+
+            CREATE TABLE cliente(
+                id INT auto_increment,
+                nombre VARCHAR (50),
+                apellidouno VARCHAR (50),
+                apellidodos VARCHAR (50),
+                email VARCHAR (50),
+                CONSTRAINT PK_cliente PRIMARY KEY (id)
+            );
+            
+             CREATE TABLE coche(
+                id INT auto_increment,
+                modelo VARCHAR (50),
+                precio DOUBLE,
+                fabricante VARCHAR(50),
+                anio INT,
+                km INT,
+                CONSTRAINT PK_coche PRIMARY KEY (id)
+            );
+            
+             CREATE TABLE ventas(
+                id INT auto_increment,
+                id_cliente INT,
+                Id_coche INT,
+                anio_de_compra INT,
+                CONSTRAINT PK_ventas PRIMARY KEY (id,id_cliente,id_coche),
+                CONSTRAINT FK_clienteventas FOREIGN KEY (id_cliente) REFERENCES cliente (id),
+                CONSTRAINT FK_cocheventas FOREIGN KEY (id_coche) REFERENCES coche (id)
+            );
